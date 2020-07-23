0153c1c144d9bff1f206abc2988bf6d9055f191d
Juan Ormelli <juan_ormelli@hotmail.com>
2020-07-22 21:49:37 -0300

    My first Dictionary Application

---
+++ ElectionProgram/Entities/Election.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectionProgram.Entities
+{
+    class Election
+    {
+
+        public string Candidates { get; set; }
+        public int Votes { get; set; }
+
+        public Election()
+        {
+
+        }
+
+        public Election(string candidates, int votes)
+        {
+            Candidates = candidates;
+            Votes = votes;
+        }
+
+        public override int GetHashCode()
+        {
+            return Candidates.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            return Candidates.Equals(obj);
+        }
+    }
+}
---
+++ ElectionProgram/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace ElectionProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter file full path: ");
+            string Path = Console.ReadLine();
+            try {
+                using (StreamReader sr = File.OpenText(Path))
+                {
+                    Dictionary<string, int> count = new Dictionary<string, int>();
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(";");
+                        string candidate = line[0];
+                        int votes = int.Parse(line[1]);
+
+                        
+                        if (count.ContainsKey(candidate))
+                        {
+
+                            count[candidate] += votes;
+
+                        }
+                        else
+                        {
+                            count[candidate] = votes;
+                        }
+
+                    }
+                    foreach (var c in count)
+                    {
+                        Console.WriteLine(c.Key + ": " + c.Value);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
